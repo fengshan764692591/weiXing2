f91c1bbef7c4e2c4409da142d375a2331f8b1d71
fengshan <fengshan@mogulinker.com>
2019-09-08 21:33:29 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,40 +1,43 @@
 //Page Object
+  import {request} from "../../request/index.js"
 Page({
   data: {
-    
+    swiperList:[],
+    navCateList: [],
+    floorList: [],
   },
-  //options(Object)
-  onLoad: function(options) {
-    
-  },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
-
-  },
-  onUnload: function() {
-
-  },
-  onPullDownRefresh: function() {
-
-  },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
-  }
+//  获取轮播图数据
+onLoad(){
+  this.getswiperList();
+  this.getnavCateList();
+  this.getfloorList();
+},
+getswiperList(){   
+  request({url: '/home/swiperdata'})
+  .then((result)=>{
+      this.setData({
+      swiperList:result
+      });
+  });
+},
+//  获取分类数据
+getnavCateList(){  
+  request({url: '/home/catitems'})
+  .then((result)=>{
+      this.setData({
+        navCateList: result
+      });
+  });
+},
+// 获取楼层数据
+getfloorList(){ 
+  request({url: '/home/floordata'})
+  .then((result)=>{
+      this.setData({
+        floorList: result
+      });
+  });
+}
 });
   
   
